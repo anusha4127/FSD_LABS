87c2f02fe31709b7759baa192bf19176cbebd59a
anusha4127 <mogilianusha412@gmail.com>
2026-08-16 16:55:24 +0530

    Added lab1

---
+++ color.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+console.log(chalk.red('M.Anusha'));
+console.log(chalk.green('B24CS297'));
+console.log(chalk.yellow('5CSE5'));
+console.log(chalk.blue('B.Tech CSE 5th Semester 3rd Year'));
